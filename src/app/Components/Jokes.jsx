f7dc462304049bb028598287dc6ae4238c421bee
mubking <adeshinamubarak6@gmail.com>
2023-08-10 01:20:47 +0100

    Add copy text change and adjust icon font size

--- src/app/Components/Jokes.jsx
+++ src/app/Components/Jokes.jsx
@@ -14,7 +14,9 @@ const Jokes = ({ slip, fetchRandomJoke }) => {
       {slip ? (
         <div className="bg-[#313A49] h-[35vh] p-10  rounded-lg shadow text-center flex flex-col items-center justify-center">
           <h2 className="text-xl font-bold mb-2 text-[#4BD194]">Advice {slip.id}</h2>
-          <p className="text-[#CEE3E9] text-xl">"{slip.advice}"</p>
+          <p className="text-[#CEE3E9] text-xl">{slip.advice}</p>
+
+
 
           <div className="three cursor-pointer">
             <img
